a5986eac9052128471d34aa9ea4ec61046c5c83e
Marco Ehrenmann <marco.ehrenmann@wearelucid.ch>
2021-02-24 14:29:33 +0100

    [Chore] nuxtImageConfig: Simplify

--- configs/nuxtImageConfig.js
+++ configs/nuxtImageConfig.js
@@ -8,15 +8,15 @@ const createNuxtImageConfig = (domains = []) => {
   // Defining breakpoints (screens) based on global breakpoints.json
   // This will be the base for any srcset when using <nuxt-image>.
   // Can then be used as: :sizes="`small:50vw medium:100vw large:400px`"
-  const imageBreakpoints = {}
+  const screens = {}
   const jsonBreakpoints = breakpointConfig.jsonBreakpoints
   for (const bp in jsonBreakpoints) {
-    imageBreakpoints[bp] = parseInt(jsonBreakpoints[bp], 10)
+    screens[bp] = parseInt(jsonBreakpoints[bp], 10)
   }
 
   return {
     domains,
-    screens: imageBreakpoints,
+    screens,
   }
 }
 
