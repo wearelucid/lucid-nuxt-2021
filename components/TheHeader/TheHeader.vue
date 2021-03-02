09483f97ccb9be8f14687330e962af2721fbc29d
Fabian Ellenberger <fabianellenberger@gmail.com>
2021-03-02 18:30:25 +0100

    feat: adding theHeader component

---
+++ components/TheHeader/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <ThePreviewIndicator v-if="$nuxt.isPreview" />
+    <TheCraftNavigation />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+}
+</script>
