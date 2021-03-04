2ed71705a7a23c1f875ea08923a1d61de0c3204a
Marco Ehrenmann <marco.ehrenmann@wearelucid.ch>
2021-03-04 17:24:52 +0100

    feat(nav): add stories for CraftVerbbNavigationListItem

---
+++ components/CraftVerbbNavigation/CraftVerbbNavigationListItem.stories.js
@@ -0,0 +1,49 @@
+import CraftVerbbNavigationListItem from './CraftVerbbNavigationListItem'
+
+export default {
+  component: CraftVerbbNavigationListItem,
+  title: 'CraftVerbbNavigationListItem',
+  decorators: [() => ({ template: '<ul><story /></ul>' })],
+}
+
+const Template = (args, { argTypes }) => ({
+  components: { CraftVerbbNavigationListItem },
+  props: Object.keys(argTypes),
+  template: '<CraftVerbbNavigationListItem v-bind="$props" />',
+})
+
+export const Internal = Template.bind({})
+Internal.args = {
+  id: '123',
+  title: 'Testseite',
+  url: 'http://localhost:3000/test-seite',
+  nodeUri: 'test-seite',
+  level: 1,
+  newWindow: '0',
+  type: 'craft\\elements\\Entry',
+  children: [],
+}
+
+export const Passive = Template.bind({})
+Passive.args = {
+  id: '234',
+  title: 'Title only (passive)',
+  url: null,
+  nodeUri: '',
+  level: 1,
+  newWindow: '0',
+  type: 'verbb\\navigation\\nodetypes\\PassiveType',
+  children: [],
+}
+
+export const External = Template.bind({})
+External.args = {
+  id: '345',
+  title: 'External Link (new tab)',
+  url: 'https://www.wearelucid.ch',
+  nodeUri: 'https://www.wearelucid.ch',
+  level: 1,
+  newWindow: '1',
+  type: '',
+  children: [],
+}
