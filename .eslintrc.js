603af2bd07e1dbcadcbd4b3f7696323ba95bd98e
Marco Ehrenmann <marco.ehrenmann@wearelucid.ch>
2021-03-04 15:57:11 +0100

    chore(eslint): update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,9 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
 }
