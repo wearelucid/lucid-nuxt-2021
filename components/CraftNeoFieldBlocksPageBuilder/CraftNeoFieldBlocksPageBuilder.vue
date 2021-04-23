f42b805cd983174bd8ea0e3ae8c06fc100305238
Marco Ehrenmann <marco.ehrenmann@wearelucid.ch>
2021-04-23 13:36:55 +0200

    feat: use typeHandle for page builder instead of __typename

--- components/CraftNeoFieldBlocksPageBuilder/CraftNeoFieldBlocksPageBuilder.vue
+++ components/CraftNeoFieldBlocksPageBuilder/CraftNeoFieldBlocksPageBuilder.vue
@@ -2,23 +2,21 @@
   <div :class="$options.name">
     <div v-for="item in items" :key="item.id" :class="`${$options.name}__item`">
       <BaseSection
-        v-if="item.__typename === 'pageBuilder_pbSection_BlockType'"
+        v-if="item.typeHandle === 'pbSection'"
         :title="item.baseTitle"
       >
         <BaseRichtext v-if="item.baseRichtext" :content="item.baseRichtext" />
       </BaseSection>
 
       <NuxtImg
-        v-if="item.__typename === 'pageBuilder_pbImage_BlockType'"
+        v-if="item.typeHandle === 'pbImage'"
         :alt="item.baseImage[0].title"
         :src="item.baseImage[0].url"
         sizes="initial:100vw small:50vw medium:500px large:768px"
       />
 
       <CraftTypedLinkField
-        v-if="
-          item.__typename === 'pageBuilder_pbLink_BlockType' && item.baseLink
-        "
+        v-if="item.typeHandle === 'pbLink' && item.baseLink"
         v-bind="item.baseLink"
       />
     </div>
