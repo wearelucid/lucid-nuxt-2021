069daaa0d51a9f6d2c7bb777e5caaeb3ca1c6afe
Jones <git@jonasscheiwiller.ch>
2021-10-15 17:02:15 +0200

    feat: add quick scaffolding for components and modules

---
+++ plop/scaffold/components/index.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const changeCase = require('change-case')
+
+module.exports = (plop, config) => {
+  plop.setHelper('toLowerCase', function (text) {
+    return text.toLowerCase()
+  })
+
+  plop.setGenerator(`${config.prefix}component`, {
+    description: 'Generate Component',
+    prompts: [
+      {
+        type: 'input',
+        message: 'Component name',
+        name: 'componentName',
+        validate: (value) => {
+          if (!value.length) return 'A component name is required.'
+          if (!value.match(/^[a-zA-Z]+$/))
+            return 'A component name can only contain letters.'
+          if (changeCase.pascalCase(value) !== value)
+            return 'A component name must be written in PascalCase.'
+          return true
+        },
+      },
+    ],
+    actions: (answers) => {
+      const actions = [
+        {
+          type: 'add',
+          path: path.resolve(
+            config.basePath,
+            'components',
+            answers.componentName,
+            answers.componentName + '.vue'
+          ),
+          templateFile: path.resolve(__dirname, 'component.hbs'),
+        },
+        {
+          type: 'add',
+          path: path.resolve(
+            config.basePath,
+            'components',
+            answers.componentName,
+            answers.componentName + '.scss'
+          ),
+          templateFile: path.resolve(__dirname, 'component.scss.hbs'),
+        },
+      ]
+
+      return actions
+    },
+  })
+}
