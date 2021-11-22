9ca136cb13c3d6b6e827accabf9e3498974d0bad
Fabian Ellenberger <fabianellenberger@gmail.com>
2021-11-22 16:18:03 +0100

    chore: update stylelint and lint files to match new rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recommended-vue',
     'stylelint-config-sass-guidelines',
+    'stylelint-config-standard-scss',
+    'stylelint-config-recommended-vue',
     'stylelint-config-prettier',
   ],
   // add your custom config here
