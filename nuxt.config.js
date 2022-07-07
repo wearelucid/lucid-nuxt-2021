53bbf60a7ac9df883b5469c392ad502e6f4307f8
Tobias Dupuch <tobias.dupuch@wearelucid.ch>
2022-07-07 15:45:31 +0200

    feat(config): disable postcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,6 +84,7 @@ export default {
   build: {
     // TODO: 'vue-link' seems not to work if not transpiled by babel. Maybe I'm missing something here.
     transpile: ['vue-link'],
+    postcss: null,
   },
 
   // Generate Options
